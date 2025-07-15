13d210a27e76e71cab80050e5df6b1c88528fed4
NQB37 <nqb37.dev@gmail.com>
2025-07-15 04:17:20 +0200

    add "Download" feature

--- frontend/segmentation-image-gui/src/components/workplace/canvas/CanvasContainer.jsx
+++ frontend/segmentation-image-gui/src/components/workplace/canvas/CanvasContainer.jsx
@@ -1,92 +1,92 @@
-import { useEffect } from 'react';
-import { useCanvasContext } from '../../../hooks/useCanvasContext';
+import { useEffect } from "react";
+import { useCanvasContext } from "../../../hooks/useCanvasContext";
 
 const CanvasContainer = () => {
-    const {
-        containerRef,
-        bgCanvasRef,
-        canvasRef,
-        maskCanvasRef,
-        scale,
-        pos,
-        totalDrawnLength,
-        startPan,
-        pan,
-        endPan,
-        handleWheelZoom,
-        handleCanvasClick,
-        startDrawing,
-        draw,
-        endDrawing,
-        annotationToggle,
-        maskToggle,
-    } = useCanvasContext();
+  const {
+    containerRef,
+    bgCanvasRef,
+    canvasRef,
+    maskCanvasRef,
+    scale,
+    pos,
+    totalDrawnLength,
+    startPan,
+    pan,
+    endPan,
+    handleWheelZoom,
+    handleCanvasClick,
+    startDrawing,
+    draw,
+    endDrawing,
+    annotationToggle,
+    maskToggle,
+  } = useCanvasContext();
 
-    // init canvas
-    useEffect(() => {
-        const container = bgCanvasRef.current.parentNode;
-        const width = container.offsetWidth;
-        const height = container.offsetHeight;
+  // init canvas
+  useEffect(() => {
+    const container = bgCanvasRef.current.parentNode;
+    const width = container.offsetWidth;
+    const height = container.offsetHeight;
 
-        bgCanvasRef.current.width = width;
-        bgCanvasRef.current.height = height;
-        canvasRef.current.width = width;
-        canvasRef.current.height = height;
-        maskCanvasRef.current.width = width;
-        maskCanvasRef.current.height = height;
-    }, []);
+    bgCanvasRef.current.width = width;
+    bgCanvasRef.current.height = height;
+    canvasRef.current.width = width;
+    canvasRef.current.height = height;
+    maskCanvasRef.current.width = width;
+    maskCanvasRef.current.height = height;
+  }, []);
 
-    return (
-        <div
-            className="relative w-5/6 overflow-hidden"
-            onMouseDown={startPan}
-            onMouseMove={pan}
-            onMouseUp={endPan}
-            onMouseLeave={endPan}
-        >
-            <div
-                ref={containerRef}
-                onWheel={handleWheelZoom}
-                className="absolute inset-0 flex items-center justify-center"
-                style={{
-                    transform: `scale(${scale}) translate(${pos.x}px, ${pos.y}px)`,
-                    transformOrigin: `${origin.x}% ${origin.y}%`,
-                }}
-            >
-                {/* Background canvas for image */}
-                <canvas
-                    ref={bgCanvasRef}
-                    className={`z-0 absolute top-0 left-0 size-full `}
-                />
-                {/* Mark canvas  */}
-                {/* <canvas
+  return (
+    <div
+      className="relative w-5/6 overflow-hidden"
+      onMouseDown={startPan}
+      onMouseMove={pan}
+      onMouseUp={endPan}
+      onMouseLeave={endPan}
+    >
+      <div
+        ref={containerRef}
+        onWheel={handleWheelZoom}
+        className="absolute inset-0 flex items-center justify-center"
+        style={{
+          transform: `scale(${scale}) translate(${pos.x}px, ${pos.y}px)`,
+          transformOrigin: `${origin.x}% ${origin.y}%`,
+        }}
+      >
+        {/* Background canvas for image */}
+        <canvas
+          ref={bgCanvasRef}
+          className={`z-0 absolute top-0 left-0 size-full `}
+        />
+        {/* Mark canvas  */}
+        {/* <canvas
                     ref={maskCanvasRef}
                     className={`z-10 absolute top-0 left-0 size-full ${
                         maskToggle ? '' : 'hidden'
                     }`}
                 /> */}
-                <canvas
-                    ref={maskCanvasRef}
-                    className={`z-10 absolute top-0 left-0 size-full`}
-                />
-                {/* Annotation canvas overlay */}
-                <canvas
-                    ref={canvasRef}
-                    className={`z-20 absolute top-0 left-0 size-full ${
-                        annotationToggle ? '' : 'hidden'
-                    }`}
-                    onClick={handleCanvasClick}
-                    onMouseDown={startDrawing}
-                    onMouseMove={draw}
-                    onMouseUp={endDrawing}
-                    onMouseLeave={endDrawing}
-                />
-            </div>
-            <div className="absolute right-0 bottom-0 w-fit bg-gray-300">
-                <p>Total: {totalDrawnLength} pixels</p>
-            </div>
-        </div>
-    );
+        <canvas
+          ref={maskCanvasRef}
+          className={`z-10 absolute top-0 left-0 size-full opacity-25`}
+        />
+        {/* Annotation canvas overlay */}
+        <canvas
+          ref={canvasRef}
+          className={`z-20 absolute top-0 left-0 size-full ${
+            annotationToggle ? "" : "hidden"
+          }`}
+          onClick={handleCanvasClick}
+          onMouseDown={startDrawing}
+          onMouseMove={draw}
+          onMouseUp={endDrawing}
+          onMouseLeave={endDrawing}
+        />
+      </div>
+      <div className="absolute right-0 bottom-0 w-fit bg-gray-300">
+        <p>Total: {totalDrawnLength} pixels</p>
+      </div>
+    </div>
+  );
 };
 
 export default CanvasContainer;
--- frontend/segmentation-image-gui/src/components/workplace/toolbox/ToolboxContainer.jsx
+++ frontend/segmentation-image-gui/src/components/workplace/toolbox/ToolboxContainer.jsx
@@ -1,93 +1,117 @@
-import axios from 'axios';
-import { useCanvasContext } from '../../../hooks/useCanvasContext';
-import CustomBtn from '../../CustomBtn';
-import CustomZoom from '../../CustomZoom';
-import BtnBrush from './BtnBrush';
-import BtnEraser from './BtnEraser';
-import BtnFill from './BtnFill';
-import BtnMove from './BtnMove';
-import { useAuthContext } from '../../../hooks/useAuthContext';
-import { toast } from 'react-toastify';
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import { useCanvasContext } from "../../../hooks/useCanvasContext";
+import CustomBtn from "../../CustomBtn";
+import CustomZoom from "../../CustomZoom";
+import BtnBrush from "./BtnBrush";
+import BtnEraser from "./BtnEraser";
+import BtnFill from "./BtnFill";
+import BtnMove from "./BtnMove";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+import BtnGreen from "../../Share/BtnGreen";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
 const ToolboxContainer = () => {
-    const {
-        canvasRef,
-        moveSelected,
-        brushSelected,
-        brushSize,
-        fillSelected,
-        eraserSelected,
-        eraserSize,
-        scale,
-        handleMove,
-        handleBrush,
-        handleBrushSize,
-        handleEraser,
-        handleEraserSize,
-        handleFill,
-        handleZoomChange,
-        handleClearCanvas,
-    } = useCanvasContext();
+  const {
+    canvasRef,
+    bgCanvasRef,
+    moveSelected,
+    brushSelected,
+    brushSize,
+    fillSelected,
+    eraserSelected,
+    eraserSize,
+    scale,
+    handleMove,
+    handleBrush,
+    handleBrushSize,
+    handleEraser,
+    handleEraserSize,
+    handleFill,
+    handleZoomChange,
+    handleClearCanvas,
+  } = useCanvasContext();
 
-    const { user } = useAuthContext();
-    const { id } = useParams();
+  const { user } = useAuthContext();
+  const { id } = useParams();
 
-    const handleSaveAnnotation = async () => {
-        const dataURL = canvasRef.current.toDataURL('image/png', 1.0);
-        try {
-            const res = await axios.patch(
-                `http://localhost:3700/api/boardRoute/${id}`,
-                { annotationImage: dataURL },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${user.token}`,
-                    },
-                },
-            );
-            toast.success('Save successfully.');
-        } catch (error) {
-            toast.error(
-                error.response?.data?.error || 'An error occurred (FE).',
-            );
+  const handleSaveAnnotation = async () => {
+    const dataURL = canvasRef.current.toDataURL("image/png", 1.0);
+    try {
+      const res = await axios.patch(
+        `http://localhost:3700/api/boardRoute/${id}`,
+        { annotationImage: dataURL },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
         }
-    };
+      );
+      toast.success("Save successfully.");
+    } catch (error) {
+      toast.error(error.response?.data?.error || "An error occurred (FE).");
+    }
+  };
 
-    return (
-        <div className="flex justify-between border-y border-black px-4 py-2">
-            <div className="flex gap-2 w-full items-center">
-                <BtnMove moveSelected={moveSelected} handleMove={handleMove} />
-                <BtnBrush
-                    brushSize={brushSize}
-                    brushSelected={brushSelected}
-                    handleBrush={handleBrush}
-                    handleBrushSize={handleBrushSize}
-                />
-                <BtnFill onClick={handleFill} isClick={fillSelected} />
-                <BtnEraser
-                    eraserSelected={eraserSelected}
-                    eraserSize={eraserSize}
-                    handleEraser={handleEraser}
-                    handleEraserSize={handleEraserSize}
-                />
-                <CustomZoom value={scale} onChange={handleZoomChange} />
-                <CustomBtn
-                    icon={'fa-solid fa-trash'}
-                    title="Eraser all"
-                    onClick={handleClearCanvas}
-                />
-            </div>
-            <div className="flex gap-2 items-center">
-                <button
-                    onClick={handleSaveAnnotation}
-                    className={`px-4 py-2 rounded bg-green-400 hover:bg-green-600 cursor-pointer`}
-                >
-                    Save
-                </button>
-            </div>
-        </div>
-    );
+  const handleDownload = async () => {
+    const zip = new JSZip();
+
+    const imageCanvas = bgCanvasRef.current;
+    const canvasCanvas = canvasRef.current;
+
+    const imageDataUrl = imageCanvas.toDataURL("image/png");
+    const canvasDataUrl = canvasCanvas.toDataURL("image/png");
+
+    const combinedCanvas = document.createElement("canvas");
+    combinedCanvas.width = imageCanvas.width;
+    combinedCanvas.height = imageCanvas.height;
+    const ctx = combinedCanvas.getContext("2d");
+    ctx.drawImage(imageCanvas, 0, 0);
+    ctx.drawImage(canvasCanvas, 0, 0);
+    const combinedDataUrl = combinedCanvas.toDataURL("image/png");
+
+    // Add images to zip
+    zip.file("image.png", imageDataUrl.split(",")[1], { base64: true });
+    zip.file("label.png", canvasDataUrl.split(",")[1], { base64: true });
+    zip.file("combined.png", combinedDataUrl.split(",")[1], { base64: true });
+
+    const blob = await zip.generateAsync({ type: "blob" });
+    saveAs(blob, "canvas.zip");
+  };
+
+  return (
+    <div className="flex justify-between border-y border-black px-4 py-2">
+      <div className="flex gap-2 w-full items-center">
+        <BtnMove moveSelected={moveSelected} handleMove={handleMove} />
+        <BtnBrush
+          brushSize={brushSize}
+          brushSelected={brushSelected}
+          handleBrush={handleBrush}
+          handleBrushSize={handleBrushSize}
+        />
+        <BtnFill onClick={handleFill} isClick={fillSelected} />
+        <BtnEraser
+          eraserSelected={eraserSelected}
+          eraserSize={eraserSize}
+          handleEraser={handleEraser}
+          handleEraserSize={handleEraserSize}
+        />
+        <CustomZoom value={scale} onChange={handleZoomChange} />
+        <CustomBtn
+          icon={"fa-solid fa-trash"}
+          title="Eraser all"
+          onClick={handleClearCanvas}
+        />
+      </div>
+      <div className="flex gap-2 items-center">
+        <BtnGreen text="Download" onClick={handleDownload} />
+        <BtnGreen text="Save" onClick={handleSaveAnnotation} width="min-w-20" />
+      </div>
+    </div>
+  );
 };
 
 export default ToolboxContainer;
