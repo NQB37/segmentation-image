169e1f0f5bb84d8f1c35aa4fe8ef5c5b422093b9
NQB37 <nqb37.dev@gmail.com>
2026-04-29 22:45:28 +0200

    style(header): adjust padding Header

--- frontend/src/components/Share/Header.jsx
+++ frontend/src/components/Share/Header.jsx
@@ -3,17 +3,19 @@ import DropdownProfile from './DropdownProfile';
 import Notification from './Notification';
 
 const Header = () => {
-    return (
-        <header className="bg-gray-300 p-4 flex justify-between items-center">
-            <Link to="/board" className="text-4xl font-bold">
-                CellSeg
-            </Link>
-            <div className="flex gap-8 items-center">
-                <Notification />
-                <DropdownProfile />
-            </div>
-        </header>
-    );
+  return (
+    <header className='bg-gray-300 py-4'>
+      <div className='container mx-auto flex justify-between'>
+        <Link to='/board' className='text-4xl font-bold'>
+          CellSeg
+        </Link>
+        <div className='flex gap-8 items-center'>
+          <Notification />
+          <DropdownProfile />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
