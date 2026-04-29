78a31e83ff240cfdc17948c4a43e0920feee5dd9
NQB37 <nqb37.dev@gmail.com>
2026-04-30 00:12:56 +0200

    feat: implement tabbed workspace sidebar

---
+++ frontend/src/components/workplace/WorkspaceSidebar.jsx
@@ -0,0 +1,37 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import LabelContainer from "./label/LabelContainer";
+import MemberContainer from "./member/MemberContainer";
+import { useLabelContext } from "../../hooks/useLabelContext";
+import { useMemberContext } from "../../hooks/useMemberContext";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+const WorkspaceSidebar = () => {
+  const { labels } = useLabelContext();
+  const { members } = useMemberContext();
+  const { user } = useAuthContext();
+
+  return (
+    <div className="w-72 border-l bg-background flex flex-col">
+      <Tabs defaultValue="labels" className="flex flex-col h-full">
+        <TabsList className="w-full justify-start rounded-none border-b bg-transparent px-2 h-10">
+          <TabsTrigger value="labels" className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-primary rounded-none shadow-none">
+            Labels
+          </TabsTrigger>
+          <TabsTrigger value="members" className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-primary rounded-none shadow-none">
+            Members
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="labels" className="flex-grow overflow-hidden m-0">
+          <LabelContainer labels={labels} />
+        </TabsContent>
+        <TabsContent value="members" className="flex-grow overflow-hidden m-0">
+          <MemberContainer 
+            members={members.filter(member => member.email !== user?.email)} 
+          />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default WorkspaceSidebar;
