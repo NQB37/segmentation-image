f0a90736ff1cb7b3fe12aca2e87475a395514c64
NQB37 <nqb37.dev@gmail.com>
2026-04-29 23:59:01 +0200

    chore: setup TooltipProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { AuthContextProvider } from './context/AuthContext';
+import { TooltipProvider } from './components/ui/tooltip';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <AuthContextProvider>
-            <App />
+            <TooltipProvider>
+                <App />
+            </TooltipProvider>
         </AuthContextProvider>
     </StrictMode>,
 );
