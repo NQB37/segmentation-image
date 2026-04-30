86924fc77edf8fe36330a623c7ed492cf905dda2
NQB37 <nqb37.dev@gmail.com>
2026-04-30 12:10:58 +0200

    feat(be): handle more env variables

--- backend/config/environement.js
+++ backend/config/environement.js
@@ -1,7 +1,14 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+const envPath =
+  process.env.NODE_ENV === 'development' ? '.env' : '.env.production';
+
+dotenv.config({ path: envPath });
 
 export default {
-    MONGO_URI: process.env.MONGO_URI,
-    DATABASE_NAME: process.env.DATABASE_NAME,
-    APP_PORT: process.env.APP_PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  PORT: process.env.PORT,
+  API_URL: process.env.API_URL,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
